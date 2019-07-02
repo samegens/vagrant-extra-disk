da754ca94349f551b90a1301e275e389f30335dd
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2019-07-02 06:53:17 +0200

    Clean up and simplified.

--- Vagrantfile
+++ Vagrantfile
@@ -1,17 +1,19 @@
 Vagrant.configure("2") do |config|
 	config.vm.define "extradisk" do |extradisk|
 		extradisk.vm.box = "ubuntu/bionic64"
-		extradisk.vm.hostname = "extradisk"
-		extradisk.vm.network "private_network", ip: "192.168.34.10"
-		extradisk.vm.boot_timeout = 60
+		extradisk.vm.provision "shell", path: "init-extra-disk.sh"
 	end
 
 	config.vm.provider "virtualbox" do |vb|
 		file_to_disk = 'D:/Virtual Machines/extra_disk.vdi'
 		unless File.exist?(file_to_disk)
+			# Create the image if it doesn't exist yet.
+			# Note that creating the image here will cause the image to be deleted on 'vagrant destroy'.
+			# If you don't want that, create the image yourself and just refer to that image.
+			# The image will be dynamic by default, the specified disk size is 1 GB.
 			vb.customize ['createhd', '--filename', file_to_disk, '--size', 1 * 1024]
 		end
-		# SCSI Port 1 and 2 are taken bij de actual VM image and the configdrive image.
+		# SCSI Port 1 and 2 are taken bij de actual VM image and the configdrive image, so we put the disk on port 3.
 		vb.customize ['storageattach', :id, '--storagectl', 'SCSI', '--port', 3, '--type', 'hdd', '--medium', file_to_disk]
 	end
 end
--- init-extra-disk.sh
+++ init-extra-disk.sh
@@ -6,37 +6,31 @@ set -u
 cp /etc/fstab ~/
 sudo cp /etc/fstab /etc/fstab.original
 
-backupdevice=sdc
-backupdisk="/dev/${backupdevice}"
-backuppartition=${backupdisk}1
+# sda and sdb are taken by the actual VM image and the configdrive so we have to use sdc.
+disk=/dev/sdc
+partition=${disk}1
 
 # Set value to be used for filesystem mount point folder
-mountpointname='data2'
-mountpoint=/media/$mountpointname
+mountpoint=/mnt/extradisk
 
 # Set value to be used for filesystem label - max length 16 chars
-fslabel=$(hostname)-$mountpointname
+fslabel=extradisk
 
-# Set value for filesystem barriers - 0 if using Premium Storage w/ ReadOnly Caching or NoCache; 1 otherwise
-b=0
-
-if [ ! -b "${backuppartition}" ]; then
-	(echo n; echo p; echo 1; echo ; echo ; echo p; echo w;) | sudo fdisk ${backupdisk}
+if [ ! -b "${partition}" ]; then
+	(echo n; echo p; echo 1; echo ; echo ; echo p; echo w;) | sudo fdisk ${disk}
 
 	# Format and label filesystem
 	# -F is used to force the creation of the filesystem here, which we can do safely since this script should only be executed the first time the VM is built.
 	# Doing this on an existing VM with data on it is a dangerous thing to do. USE WITH CARE
 	echo Creating ext4 filesystem
-	sudo mkfs.ext4 ${backuppartition} -L ${fslabel} -F -F
-	echo Checking ext4 filesystem
-	sudo fsck ${backuppartition}
-	echo ${backuppartition} is now an ext4 file system
+	sudo mkfs.ext4 ${partition} -L ${fslabel} -F
+	echo ${partition} is now an ext4 file system
 else
 	echo Disk was already formatted.
 fi
 
 # Get value of UUID for new filesystem
-uuid=$(sudo blkid -p ${backuppartition} | grep -oP '[-a-z0-9]{36}')
+uuid=$(sudo blkid -p ${partition} | grep -oP '[-a-z0-9]{36}')
 echo uuid = $uuid
 
 # Create mount point folder
@@ -54,9 +48,3 @@ echo Temp fstab moved to /etc/fstab
 # Mount all unmounted filesystems
 sudo mount -a
 echo All unmounted filesystems are now mounted
-
-# After initial provisioning, use these commands to obtain disk device or UUID of filesystem based on label
-#disk=$(blkid -L ${fslabel})
-#uuid=$(blkid | grep "LABEL=\"${fslabel}\"" | grep -oP '[-a-z0-9]{36}')
-
-exit 0
